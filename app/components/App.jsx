bdca9391eb66f207be3b8aff26950890ae69a851
Adrian Smith <sfyire@gmail.com>
2016-05-14 22:05:25 +0100

    Introduce airbnb eslinting

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import Note from './Note.jsx';
 
-export default class App extends React.Component {
-	render() {
-		return <Note />;
-	}
+export default class App  // eslint-disable-line react/prefer-stateless-function
+ extends React.Component {
+  render() {
+    return <Note />;
+  }
 }
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,10 +3,10 @@ const merge = require('webpack-merge');
 const webpack = require('webpack');
 const NpmInstallPlugin = require('npm-install-webpack-plugin');
 
-const TARGET = process.env.npm_lifecycle_event; 
+const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
   app: path.join(__dirname, 'app'),
-  build: path.join(__dirname, 'build')
+  build: path.join(__dirname, 'build'),
 };
 
 process.env.BABEL_ENV = TARGET;
@@ -15,29 +15,29 @@ const common = {
   // Entry accepts a path or an object of entries. We'll be using the
   // latter form given it's convenient with more complex configurations.
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['', '.js', '.jsx'],
   },
   entry: {
-    app: PATHS.app
+    app: PATHS.app,
   },
   output: {
     path: PATHS.build,
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   module: {
     loaders: [
       {
         test: /\.css$/,
         loaders: ['style', 'css'],
-        include: PATHS.app
+        include: PATHS.app,
       },
       {
         test: /\.jsx?$/,
         loaders: ['babel?cacheDirectory'],
-        include: PATHS.app
-      }
-    ]
-  }
+        include: PATHS.app,
+      },
+    ],
+  },
 };
 
 if (TARGET === 'start' || !TARGET) {
@@ -51,14 +51,14 @@ if (TARGET === 'start' || !TARGET) {
       process: true,
       stats: 'errors-only',
       host: process.env.HOST,
-      port: process.env.PORT
+      port: process.env.PORT,
     },
     plugins: [
       new webpack.HotModuleReplacementPlugin(),
       new NpmInstallPlugin({
-        save: true
-      })
-    ]
+        save: true,
+      }),
+    ],
   });
 }
 
